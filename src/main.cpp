efa9dcf39fbb703f7c02ff6c92225587a702c2e1
rhoentier <sjendrny@posteo.de>
2022-04-03 23:33:51 +0200

    fixes and refactorings

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,25 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include <algorithm>
+#include <vector>
 
-#define PIN 14
-#define LED_COUNT 30
+#define PIN 5
+#define LED_COUNT 59
 
-void shot_sound();
-void get_distance();
-void check_if_on_off_initialized();
-void set_pixels();
+float get_distance();
+float detect_first_hand_contact();
+void set_pixels(float last_distances[5]);
+void light_led(int led_index);
+float maesure();
 
 // Initialisierung Ultraschallsensor
-int trigger = 15;
-int echo = 4;
-long dauer = 0;
-long loop_distance = 0;
-long initial_distance = 0;
-float schallgeschwindigkeit = 0.0342;
-float led_distance = 1.66666;
-bool initial_on_off = false;
-int loop_led = 0;
-int initial_led = 0;
-int last_led = 0;
-bool imprecise_measure = false;
+const int trigger = 15;
+const int echo = 4;
+const float schallgeschwindigkeit = 0.0342;
+const float led_distance = 1.66666;
 
 bool pixel_status[LED_COUNT];
+int last_led = 0;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, PIN, NEO_RGBW + NEO_KHZ800);
 
@@ -45,140 +41,123 @@ void setup()
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-
-  check_if_on_off_initialized();
-  if (initial_on_off)
+  float initial_distance = detect_first_hand_contact();
+  float last_distances[5] = { 0 , 0 , 0 , 0 , 0 };
+  // TODO es muss überprüft werden, ob die Hand in einem Bereich ist, der angeschaltet oder ausgeschaltet ist
+  // je nach Fall muss dieser Bereich aus- oder angeschaltet werden
+  if ( initial_distance != 0 )
   {
-    initial_led = round(initial_distance / led_distance);
-    Serial.println(initial_led);
-    strip.setPixelColor(initial_led, strip.Color(0, 100, 0, 0));
-    pixel_status[initial_led] = true;
-    last_led = initial_led;
-    strip.show();
+    int initial_led = round(initial_distance / led_distance);
+    light_led(initial_led);
+    last_distances[0] = initial_distance;
+    while (last_distances[0] < 98 || last_distances[0] > 102)
+    {
+      last_distances[4] = last_distances[3];
+      last_distances[3] = last_distances[2];
+      last_distances[2] = last_distances[1];
+      last_distances[1] = last_distances[0];
+      last_distances[0] = get_distance();
+      set_pixels(last_distances);
+    }
+  delay(100);
   }
-
-  while (loop_distance != 0)
-  {
-    // Jetzt muss noch eine Funktion geschrieben werden um die Lampen aus zu machen
-    set_pixels();
-  }
-
-  Serial.println("war null");
-  initial_on_off = false;
-  loop_distance = 0;
   delay(100);
 }
 
-void check_if_on_off_initialized()
+float detect_first_hand_contact()
 {
-  shot_sound();
-  dauer = pulseIn(echo, HIGH);
-  long distance_first_check = (dauer / 2) * schallgeschwindigkeit;
+  float first_distance = get_distance();
   delay(200);
-
-  shot_sound();
-  dauer = pulseIn(echo, HIGH);
-  long distance_second_check = (dauer / 2) * schallgeschwindigkeit;
-  if (abs((distance_second_check - distance_first_check)) <= 2 && distance_first_check < 50 && distance_first_check > 0)
+  float second_distance = get_distance();
+  if ( (first_distance - second_distance) < 1) 
   {
-    initial_distance = distance_first_check;
-    loop_distance = distance_first_check;
-    initial_on_off = true;
+    if (first_distance < 0 || first_distance > 100)
+    {
+      return 0;
+    }
+    else 
+    {
+      return (first_distance + second_distance) / 2;
+    }
   }
+  else return 0;
 }
 
-void get_distance()
+float get_distance()
 {
-  long first_maesured_distance;
-  long second_maesured_distance;
-  long third_maesured_distance;
-
-  shot_sound();
-  dauer = pulseIn(echo, HIGH);
-  first_maesured_distance = (dauer / 2) * schallgeschwindigkeit;
-
-  shot_sound();
-  dauer = pulseIn(echo, HIGH);
-  second_maesured_distance = (dauer / 2) * schallgeschwindigkeit;
-  shot_sound();
-
-  dauer = pulseIn(echo, HIGH);
-  third_maesured_distance = (dauer / 2) * schallgeschwindigkeit;
-
-  if (first_maesured_distance == second_maesured_distance && first_maesured_distance == third_maesured_distance)
-  {
-    loop_distance = first_maesured_distance;
-    imprecise_measure = false;
-  }
-  else if (first_maesured_distance != second_maesured_distance && first_maesured_distance == third_maesured_distance)
-  {
-    loop_distance = first_maesured_distance;
-    imprecise_measure = true;
-  }
-  else if (first_maesured_distance == second_maesured_distance && first_maesured_distance != third_maesured_distance)
-  {
-    loop_distance = first_maesured_distance;
-    imprecise_measure = true;
-  }
-
-  else if (first_maesured_distance != second_maesured_distance && second_maesured_distance == third_maesured_distance)
-  {
-    loop_distance = second_maesured_distance;
-    imprecise_measure = true;
-  }
-  else
-  {
-    loop_distance = 1000;
-  }
-
-  if (loop_distance > 50 && loop_distance < 55)
-  {
-    loop_distance = 0;
-  }
+  // use mean as distance
+  float measurements[5] = { maesure(), maesure(), maesure(), maesure(), maesure() };
+  std::sort(measurements, measurements+5);
+  return measurements[3];
 }
 
-void shot_sound()
+float maesure()
 {
   digitalWrite(trigger, LOW);
   delay(5);
   digitalWrite(trigger, HIGH);
   delay(10);
   digitalWrite(trigger, LOW);
+  float dauer = pulseIn(echo, HIGH);
+  return (dauer / 2) * schallgeschwindigkeit;
 }
 
-void set_pixels()
+// TODO: Hier muss die Funktion noch schlau gemacht werden.
+// Es müssen realistische Werte aus den Messungen davor abgeleitet werden
+void set_pixels(float last_distances[5])
 {
-  get_distance();
-    if (loop_distance != 0 && loop_distance != 1000)
+  float loop_distance = last_distances[0];
+  if (loop_distance != 0 && loop_distance != 1000)
+  {
+    int loop_led = round(loop_distance / led_distance);
+    if ((loop_led - last_led) == 1)
     {
-      loop_led = round(loop_distance / led_distance);
-      if ((loop_led - last_led) == 1)
+      if (pixel_status[loop_led - 1] || pixel_status[loop_led + 1])
       {
-        if (pixel_status[loop_led - 1] || pixel_status[loop_led + 1])
-        {
-          strip.setPixelColor(loop_led, strip.Color(0, 100, 0, 0));
-          pixel_status[loop_led] = true;
-          strip.show();
-        }
-      }
-      else
-      {
-        if (loop_led == last_led)
-        {
-        }
-        else if (loop_led < last_led)
-        {
-          for (int i = loop_led; i < last_led; i++)
-            strip.setPixelColor(i, strip.Color(0, 100, 0, 0));
-        }
-        else if (loop_led > last_led)
-        {
-          for (int i = last_led + 1; i <= loop_led; i++)
-            strip.setPixelColor(i, strip.Color(0, 100, 0, 0));
-        }
+        strip.setPixelColor(loop_led, strip.Color(0, 100, 0, 0));
+        pixel_status[loop_led] = true;
         strip.show();
       }
-      delay(200);
     }
+    else
+    {
+      if (loop_led == last_led)
+      {
+        // Mach hier mal nix
+      }
+      else if (loop_led < last_led)
+      {
+        for (int i = loop_led; i < last_led; i++)
+          strip.setPixelColor(i, strip.Color(0, 100, 0, 0));
+      }
+      else if (loop_led > last_led)
+      {
+        for (int i = last_led + 1; i <= loop_led; i++)
+          strip.setPixelColor(i, strip.Color(0, 100, 0, 0));
+      }
+      strip.show();
+    }
+    delay(200);
+    }
+}
+
+void light_led(int led_index)
+{
+  strip.setPixelColor(led_index, strip.Color(0, 100, 0, 0));  
+  pixel_status[led_index] = true;
+
+  if ( led_index > 2 && led_index < (LED_COUNT - 3))
+  {
+    if ( !pixel_status[led_index - 1])
+    {
+    strip.setPixelColor(led_index - 2, strip.Color(0, 5, 0, 0));
+    strip.setPixelColor(led_index - 1, strip.Color(0, 15, 0, 0));  
+    }
+    if ( !pixel_status[led_index + 1])
+    {
+    strip.setPixelColor(led_index + 1, strip.Color(0, 5, 0, 0));  
+    strip.setPixelColor(led_index + 2, strip.Color(0, 15, 0, 0));  
+    }
+  }
+  strip.show();
 }
